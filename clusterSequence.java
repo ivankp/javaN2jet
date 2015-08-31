c3d02a36f810e12da75e796d7af9b3a72ea9b7fb
ivankp <ivan.pogrebnyak@gmail.com>
2015-08-31 16:49:04 -0500

    Problem when switching from using grid

--- clusterSequence.java
+++ clusterSequence.java
@@ -181,7 +181,7 @@ class clusterSequence {
       nphi = (int)(twopi/R);
       d = twopi/nphi;
       r = d/2;
-      nrap = (int)(max_rap/r) + 1;
+      nrap = 2*((int)(max_rap/r) + 1);
       this.max_rap = nrap*r;
 
       tiles = new tile[nphi][nrap];
@@ -231,11 +231,11 @@ class clusterSequence {
         tu = ( sq(p.phi-p.t.phic-r) < p.Rij ),
         tl = ( sq(p.rap-p.t.rapc+r) < p.Rij ),
         tr = ( sq(p.rap-p.t.rapc-r) < p.Rij );
-        
+
       if (p.t.irap!=0) {
         if (tl) { within_tile(p, tiles[p.t.iphi][p.t.irap-1], both);
-          if (tu) within_tile(p, tiles[nphi-p.t.iphi==1 ? 0 : p.t.iphi+1][p.t.irap-1], both);
           if (td) within_tile(p, tiles[p.t.iphi==0 ? nphi-1 : p.t.iphi-1][p.t.irap-1], both);
+          if (tu) within_tile(p, tiles[nphi-p.t.iphi==1 ? 0 : p.t.iphi+1][p.t.irap-1], both);
         }
       }
 
@@ -244,8 +244,8 @@ class clusterSequence {
 
       if (nrap-p.t.irap!=1) {
         if (tr) { within_tile(p, tiles[p.t.iphi][p.t.irap+1], both);
-          if (tu) within_tile(p, tiles[nphi-p.t.iphi==1 ? 0 : p.t.iphi+1][p.t.irap+1], both);
           if (td) within_tile(p, tiles[p.t.iphi==0 ? nphi-1 : p.t.iphi-1][p.t.irap+1], both);
+          if (tu) within_tile(p, tiles[nphi-p.t.iphi==1 ? 0 : p.t.iphi+1][p.t.irap+1], both);
         }
       }
 
@@ -255,7 +255,7 @@ class clusterSequence {
   // ****************************************************************
   // clustering function ********************************************
   public List<ParticleD> cluster(List<ParticleD> particles) {
-    int n = particles.size();
+    final int n = particles.size();
     num = 0; // start assigning pseudoJet id from 0
 
     // initialize the grid
@@ -303,7 +303,7 @@ class clusterSequence {
     // loop until pseudoJets are used up ------------------
     while (first != null) {
 
-      if (n<50) use_grid = false;
+      //if (n<50) use_grid = false;
 
       p = first;
       double dist = p.diB;
@@ -408,7 +408,7 @@ class clusterSequence {
 
       }
 
-      --n;
+      //--n;
 
     }
 
