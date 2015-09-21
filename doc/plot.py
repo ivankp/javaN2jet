420227a9a4272c3a559ef82e51d626b263e06286
ivankp <ivan.pogrebnyak@gmail.com>
2015-09-21 02:18:04 -0500

    benchmark edits

--- doc/plot.py
+++ doc/plot.py
@@ -3,6 +3,7 @@
 import numpy as np
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
+from matplotlib import gridspec
 
 # example data
 x = np.array([2,4,8,16,32,64,128,256,512,1024])
@@ -32,56 +33,65 @@ n2err = np.array([
 ])
 
 fjy = np.array([
-0.989182,
-1.58708,
-3.4148,
-10.9166,
-45.645,
-41.4099,
-81.3343,
-192.879,
-504.688,
-1163.61
+1.06291,
+1.67782,
+3.53681,
+11.5995,
+48.4113,
+44.0311,
+85.8257,
+204.854,
+538.002,
+1236.26
 ])
 fjerr = np.array([
-0.628493,
-0.624047,
-0.542491,
-1.24241,
-6.22873,
-2.62773,
-4.05763,
-6.11079
+0.858589,
+0.851119,
+0.98387,
+2.2595,
+7.44948,
+6.00628,
+6.24451,
+19.1661,
+52.984,
+86.091
 ])
 
-def fpow(x, a, b): return np.exp(b)*pow(x,a)
 def fpoly1(x, a, b): return a*x+b
-#def fpoly01(x, a): return a*x
 def fpoly2(x, a, b, c): return a*x*x + b*x + c
-#def fpoly3(x, a, b, c, d): return a*x*x*x + b*x*x + c*x + d
 
-par1, cov1 = curve_fit(fpoly1, np.log(x), np.log(n2y))
-#par2, cov2 = curve_fit(fpoly10, x, y)
-par3, cov3 = curve_fit(fpoly2, x, n2y)
-#par4, cov4 = curve_fit(fpoly3, x, y)
+par1, cov1 = curve_fit(fpoly1, x[:5], n2y[:5])
+par2, cov2 = curve_fit(fpoly2, x[4:], n2y[4:])
 
-fig, ax = plt.subplots()
-ax.errorbar(x, n2y, yerr=n2err, fmt='bo', label='N2jet Java')
-ax.errorbar(x, fjy, yerr=fjerr, fmt='mo', label='FastJet')
-plt.plot(x, fpow(x, *par1), 'r-',
-         label="fit $%.3fN^{%.3f}$" % (np.exp(par1[1]),par1[0]))
-#plt.plot(x, fpoly01(x, *par2), 'y-',
-#         label="fit $%.3fn$" % (par2[0]))
-plt.plot(x, fpoly2(x, *par3), 'g-',
-         label="fit $%.3fN^{2}+%.3fN+%.3f$" % (par3[0],par3[1],par3[2]))
-#plt.plot(x, fpoly3(x, *par4), 'y-',
-#         label="fit $%.3en^{3}+%.3fn^{2}+%.3fn+%.3f$" % (par4[0],par4[1],par4[2],par4[3]))
-plt.loglog(basex=2,nonposy='clip')
-ax.set_title('Benchmark antikt 0.4 $N$ 100000')
-plt.xlabel('number of particles per event, $N$')
-plt.ylabel('mean single event clusterization time, mks')
+fig = plt.figure(figsize=(8, 6))
+gs = gridspec.GridSpec(2, 1, height_ratios=[3, 1], hspace=0)
 
+ax1 = plt.subplot(gs[0])
+ax1.loglog(basex=2,nonposy='clip')
+ax1.errorbar(x, n2y, yerr=n2err, fmt='bo', label='N2jet Java')
+ax1.errorbar(x, fjy, yerr=fjerr, fmt='mo', label='FastJet C++')
+ax1.plot(x[:5], fpoly1(x[:5], *par1), 'r-',
+         label="fit $%.1fN+%.1f$" % (np.exp(par1[1]),par1[0]))
+ax1.plot(x[4:], fpoly2(x[4:], *par2), 'g-',
+         label="fit $%.4fN^{2}+%.2fN+%.0f$" % (par2[0],par2[1],par2[2]))
+ax1.set_ylim([5e-1, 1e4])
+ax1.set_ylabel('mean single event clusterization time, mks')
 plt.legend(loc=2)
 
-plt.savefig('benchmark.pdf')
+ax2 = plt.subplot(gs[1], sharex=ax1)
+ratio = n2y/fjy
+avg = np.mean(ratio)
+print(avg)
+ax2.scatter(x, ratio)
+ax2.set_ylim([0, 5.5])
+ax2.set_yticks(np.arange(0, 6, 1))
+ax2.axhline(avg, linestyle='--')
+ax2.set_ylabel('$t_\mathtt{N2}/t_\mathtt{FJ}$', fontsize=18)
 
+plt.xlim([1.7, 1300])
+for tick in ax2.xaxis.get_major_ticks():
+    tick.label.set_fontsize(14)
+plt.xlabel('number of particles per event', fontsize=15)
+
+plt.tight_layout()
+plt.savefig('benchmark.pdf')
